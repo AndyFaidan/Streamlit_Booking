bb7582651f1687e7ba5248141540d41887589142
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2026-04-30 21:52:05 +0700

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,16 +1,35 @@
-
-import sqlite3, os
+import sqlite3
+import os
 
 DB_PATH = "data/database.db"
 
+# ======================
+# CONNECT DATABASE
+# ======================
 def get_connection():
     os.makedirs("data", exist_ok=True)
     return sqlite3.connect(DB_PATH, check_same_thread=False)
 
+
+# ======================
+# INIT DATABASE
+# ======================
 def init_db():
     conn = get_connection()
     c = conn.cursor()
 
+    # USERS
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT,
+        full_name TEXT,
+        role TEXT
+    )
+    """)
+
+    # AKUN
     c.execute("""
     CREATE TABLE IF NOT EXISTS akun_nusuk (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,11 +40,13 @@ def init_db():
         gender TEXT,
         type TEXT,
         name_identity TEXT,
+        group_id TEXT,
         status TEXT,
         checklist BOOLEAN
     )
     """)
 
+    # BOOKING
     c.execute("""
     CREATE TABLE IF NOT EXISTS booking (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
