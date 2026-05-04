fc1dd5fe9fca9fc50fea558d2c81feee31581c02
andy sofyan guspriyanto <129332680+AndyFaidan@users.noreply.github.com>
2026-05-04 20:10:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ div[data-testid="stContainer"] {
     background: white;
     padding: 20px;
     border-radius: 12px;
-    border: 1px solid #e0e0e0;
+    border: 1px solid #545454;
 }
 
 /* BUTTON */
